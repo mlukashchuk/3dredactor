0b48ee226c41981d1e98e81a9033baefd9dc2fc2
mlukashchuk <mikhail.lukashchuk@mail.ru>
2024-02-09 16:19:45 +0300

    feat: add design

---
+++ program/design.cpp
@@ -0,0 +1,18 @@
+class ModelingAndDesignModule {
+public:
+    void polyModelingTools() {
+        // Работа с полигональным моделированием
+    }
+
+    void splineModelingTools() {
+        // Работа с Spline-моделированием
+    }
+
+    void nurbsModelingTools() {
+        // Работа с NURBS-моделированием
+    }
+
+    void sculptingTools() {
+        // Работа со скульптингом
+    }
+};

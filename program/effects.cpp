901cc5bedfdd74638ebc98989a04ad14c5e6d525
mlukashchuk <mikhail.lukashchuk@mail.ru>
2024-02-09 21:38:28 +0300

    Update effects.cpp

--- program/effects.cpp
+++ program/effects.cpp
@@ -1,1 +1,10 @@
-Испорченные данные
+class EffectsAndSimulationModule {
+public:
+    void particleEffects() {
+        // Частицы и спецэффекты
+    }
+
+    void dynamicSimulation() {
+        // Динамические симуляции
+    }
+};

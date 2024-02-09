c4ccae52ab5d1043d67ff6f98813118eb5fb382a
mlukashchuk <mikhail.lukashchuk@mail.ru>
2024-02-09 16:20:30 +0300

    feat: add textures

---
+++ program/textures.cpp
@@ -0,0 +1,14 @@
+class TexturingAndMaterialsModule {
+public:
+    void materialEditor() {
+        // Работа с редактором материалов
+    }
+
+    void uvUnwrappingTools() {
+        // Работа с инструментами UV-развертки
+    }
+
+    void pbrMaterialSupport() {
+        // Поддержка PBR-материалов
+    }
+};

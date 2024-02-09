4957c59687f5f394182cc241a889a18faa129725
mlukashchuk <mikhail.lukashchuk@mail.ru>
2024-02-09 16:18:37 +0300

    feat: add interface

---
+++ program/interface.cpp
@@ -0,0 +1,14 @@
+class UserInterfaceModule {
+public:
+    void setupWorkspace() {
+        // Настройка рабочего пространства
+    }
+
+    void setupToolbarsAndMenus() {
+        // Создание и настройка панелей инструментов и меню
+    }
+
+    void provideLearningMaterialsAndTips() {
+        // Предоставление обучающих материалов и подсказок
+    }
+};

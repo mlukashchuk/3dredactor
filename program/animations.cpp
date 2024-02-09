e085b41fbd3a84eedc342bd00a2a89f05840e4bb
mlukashchuk <mikhail.lukashchuk@mail.ru>
2024-02-09 16:22:53 +0300

    feat: add animations

---
+++ program/animations.cpp
@@ -0,0 +1,14 @@
+class AnimationAndMotionModule {
+public:
+    void skeletonAnimationSystem() {
+        // Система скелетной анимации
+    }
+
+    void morphingAndDynamicAnimation() {
+        // Морфинг и динамическая анимация
+    }
+
+    void clothAndFluidSimulation() {
+        // Симуляция тканей и жидкостей
+    }
+};

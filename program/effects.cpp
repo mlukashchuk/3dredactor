83a3836a64ba960e95904ef325d9e35fc0113d08
mlukashchuk <mikhail.lukashchuk@mail.ru>
2024-02-09 21:16:01 +0300

    Made irreparable changes

--- program/effects.cpp
+++ program/effects.cpp
@@ -1,10 +1,1 @@
-class EffectsAndSimulationModule {
-public:
-    void particleEffects() {
-        // Частицы и спецэффекты
-    }
-
-    void dynamicSimulation() {
-        // Динамические симуляции
-    }
-};
+Испорченные данные
